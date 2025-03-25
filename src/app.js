e3faf5f15823d59359c6f0a210f20b99629b618d
Carolina Castillo Lucas <carolina_c.l@hotmail.com>
2025-03-25 11:24:20 +0000

    codigo recolocado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+
+  let pronoun = ['mi', 'nuestra', 'tu'];
+  let adj = ['propia', 'nueva', 'vieja'];
+  let what = ['pagina', 'web', 'ventana'];
+  let domain = ['.com', '.es', '.io', '.dev'];
+
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let w of what) {
+        for (let d of domain) {
+          console.log(`${p}${a}${w}${d}`);
+        }
+      }
+    }
+  }
 };
